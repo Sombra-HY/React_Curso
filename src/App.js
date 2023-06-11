30ab4c5fae3af357226973c6c9f324859cc49ff2
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-06-10 22:20:37 -0300

    LyfeCicle Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+
+class App extends Component{
+
+  state = {
+    counter: 0
+    ,
+    post: [
+      {
+        id:1,
+        title: "valor 1",
+        body:"copor 1"
+      },
+      {
+        id:2,
+        title: "valor 2",
+        body:"copor 2"
+      }
+    ]
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  timeOutUpdate = null;
+
+  // atualiza ao componente mudar (Ciclo)
+  componentDidUpdate(){
+    this.changeCounter();
+  }
+
+  //modifica o estado atual
+  changeCounter = () =>{
+    const {post, counter} = this.state;
+
+    this.timeOutUpdate = setTimeout(()=>{
+      this.setState({counter: counter+1, post: post});
+    },50);
+  }
+
+  //ao carregar a pagina muda componente
+  componentDidMount() {
+    this.changeCounter();
+  }
+
+  // desmontando o componente
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate)
+  }
+
+
+  render() {
+    const {post,counter} = this.state;
+
+    return (
+      <div className="App">
+        <h1> {counter} </h1>
+        {post.map(px => (
+            <div key={px.id}>
+              <h1 > {px.title}</h1>
+              <p > {px.body}</p>
+            </div>
+            )
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
